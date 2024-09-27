9fd3dfebb3150f7c26b43e8d48566af20b7a6885
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-09-27 17:25:22 +0700

    S

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+namespace RANSANMOI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             bool gameOver = false;
+            Thread _game = new Thread(SnakeControl.ListenKey);
+            _game.Start();
+            while(!gameOver)
+            {
+                try
+                {
+                Console.Clear();
+                SnakeControl.Drawboard();
+                SnakeControl.setUpBoard();
+                SnakeControl.MoveSnakeHead();
+                SnakeControl.EatFood();
+                SnakeControl.SpawnBody();
+                SnakeControl.PopUpfood();
+                SnakeControl.ShowPoint();
+                //Task.Delay(SnakeControl.speed).Wait();
+                Thread.Sleep(SnakeControl.speed);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error at"+ ex);
+                }
+            }
+            Console.WriteLine("Game Over!");
+               
+                
+                
+            
+            
+            
+            
+            
+
+             
+        }
+    }
+}
